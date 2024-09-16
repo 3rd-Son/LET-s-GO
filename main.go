4a0102d20b7dfc6a187039a04e41aa79208f1f36
3rdSon <nnajivictorious@gmail.com>
2024-09-16 13:02:07 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	// "time"
+)
+
+// type Student struct {
+//     FirstName string
+//     LastName string
+//     RegistrationNumber int
+//     Age int
+// }
+
+
+
+func main() {
+    isTrue := false
+
+    if isTrue {
+        fmt.Println("It is a", isTrue)
+    }else {
+        fmt.Println("It is", isTrue)
+    }
+}
